8c9d887c207a86d2090f418b20b933490649dbdd
mrtoy-me <118446898+mrtoy-me@users.noreply.github.com>
2025-07-16 20:48:12 +1000

    Update __init__.py

--- components/tm1651/__init__.py
+++ components/tm1651/__init__.py
@@ -53,10 +53,8 @@ async def to_code(config):
     cg.add(var.set_dio_pin(dio_pin))
     cg.add(var.set_max_levels(config[CONF_MAX_LEVELS]))
 
-upper = config.get(CONF_MAX_LEVELS)
-
 validate_brightness = cv.enum(TM1651_BRIGHTNESS_OPTIONS, int=True)
-validate_level = cv.All(cv.int_range(min=0, max=upper))
+validate_level = cv.All(cv.int_range(min=0, max=7))
 validate_level_percent = cv.All(cv.int_range(min=0, max=100))
 
 BINARY_OUTPUT_ACTION_SCHEMA = maybe_simple_id(
